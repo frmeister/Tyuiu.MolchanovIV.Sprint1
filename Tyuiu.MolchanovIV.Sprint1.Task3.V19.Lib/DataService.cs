71971e02371b20bbe8850ac90b9a6fef81e3ff43
MolchanovIV <Tyuiu.MolchanovIV@outlook.com>
2025-10-02 16:45:11 +0500

    Task3 again

--- Tyuiu.MolchanovIV.Sprint1.Task3.V19.Lib/DataService.cs
+++ Tyuiu.MolchanovIV.Sprint1.Task3.V19.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using System.Runtime.Serialization;
+using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.MolchanovIV.Sprint1.Task3.V19.Lib
 {
@@ -6,29 +7,23 @@ namespace Tyuiu.MolchanovIV.Sprint1.Task3.V19.Lib
     {
         public bool ElephCanMove(double x1, double x2, double y1, double y2)
         {
+            double z;  // Расстояние которое преодолел слон по Х
             if (x2 > x1)
             {
+                z = x2 - x1;
                 if (y2 > y1)
-                {
-                    if ((x2 == (x1 + 2)) && (y2 == (y1 + 1))) return true;
-                    if ((x2 == (x1 + 1)) && (y2 == (y1 + 2))) return true;
-                } else
-                {
-                    if ((x2 == (x1 + 2)) && (y2 == (y1 - 1))) return true;
-                    if ((x2 == (x1 + 1)) && (y2 == (y1 - 2))) return true;
-                }
-            } else
+
+                    if ((y2 - y1) == z) return true;
+                    else if ((y1 - y2) == z) return true;
+
+            }
+            else
             {
+                z = x1 - x2;
                 if (y2 > y1)
-                {
-                    if ((x2 == (x1 - 2)) && (y2 == (y1 + 1))) return true;
-                    if ((x2 == (x1 - 1)) && (y2 == (y1 + 2))) return true;
-                }
-                else
-                {
-                    if ((x2 == (x1 - 2)) && (y2 == (y1 - 1))) return true;
-                    if ((x2 == (x1 - 1)) && (y2 == (y1 - 2))) return true;
-                }
+
+                    if ((y2 - y1) == z) return true;
+                    else if ((y1 - y2) == z) return true;
             }
                 return false;
         }
