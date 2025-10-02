abcc38afad5b170da3fe686788410d6f8e51a8e2
MolchanovIV <Tyuiu.MolchanovIV@outlook.com>
2025-10-02 17:56:24 +0500

    Task4 Peredel

--- Tyuiu.MolchanovIV.Sprint1.Task4.V10.Lib/DataService.cs
+++ Tyuiu.MolchanovIV.Sprint1.Task4.V10.Lib/DataService.cs
@@ -6,9 +6,7 @@ namespace Tyuiu.MolchanovIV.Sprint1.Task4.V10.Lib
     {
         public double Calculate(double a)
         {
-            double Degree = a * (Math.PI / 180.0); // Переводим угол с градусов на радианы
-
-            return Math.Round(((1 + Math.Cos(Degree)) / (Math.Pow(Math.Sin(Degree), 2))), 3);
+            return Math.Round(((1 + Math.Cos(a)) / (Math.Pow(Math.Sin(a), 2))), 3);
         }
     }
 }
